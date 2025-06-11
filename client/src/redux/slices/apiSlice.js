8ad5bec55f8e82b7a6574a0440c96eb6c8b1482b
aryanvats2010 <aryanvats512@gmail.com>
2025-06-11 21:18:22 +0530

    sec commit

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "https://wenapp.onrender.com";
+const API_URL = "http://localhost:5000/api";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
