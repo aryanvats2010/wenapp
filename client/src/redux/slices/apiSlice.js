f60038afffe88c321b347328def883c61af219c9
aryanvats2010 <aryanvats512@gmail.com>
2025-06-12 03:08:07 +0530

    third commit

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "http://localhost:5000/api";
+// ✅ Correct (dynamic from .env)
+const API_URL = import.meta.env.VITE_API_BASE_URL + "/api";
+
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
